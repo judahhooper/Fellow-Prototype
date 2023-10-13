c48bbef195f9cb7ba4bfdf52188b139eadfc0910
Judah Hooper <judahhooper@icloud.com>
2023-10-13 22:31:28 +0100

    nativewind

--- App.tsx
+++ App.tsx
@@ -4,6 +4,12 @@ import RootNavigator from './navigations/Navigator';
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 import { MenuProvider } from 'react-native-popup-menu';
 import 'react-native-gesture-handler';
+import { NativeWindStyleSheet } from 'nativewind';
+import React from 'react';
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
